9e4cbfb25e1955dd3b512356086aec7d3bc7ffd5
savasduzgun <savasdzgn@gmail.com>
2024-08-28 14:34:53 +0300

    Vehicle oluşturuldu.

---
+++ Parking.Models/Vehicle.cs
@@ -0,0 +1,9 @@
+﻿namespace Parking.Models
+{
+    public class Vehicle : BaseModel
+    {
+        public string LicensePlate { get; set; }
+        public string Name { get; set; }
+        public double Odometer { get; set; }
+    }
+}
