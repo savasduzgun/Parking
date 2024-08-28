ff2e51b784828ffac0fc7954e83d3a5dc6fc462a
savasduzgun <savasdzgn@gmail.com>
2024-08-28 16:38:55 +0300

    Policy modeli eklendi.

---
+++ Parking.Models/Policy.cs
@@ -0,0 +1,9 @@
+﻿namespace Parking.Models
+{
+    public class Policy : BaseModel
+    {
+        public string Name { get; set; }
+        public string CompanyName { get; set; } //SigortaFirması
+        public DateTime Validity { get; set; } //GeçerlilikTarihi
+    }
+}
