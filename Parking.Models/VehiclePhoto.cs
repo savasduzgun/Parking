fdb2b2512b49ec889d7e06f126df160054f024e8
savasduzgun <savasdzgn@gmail.com>
2024-08-28 15:55:52 +0300

    VehiclePhoto modeli oluşturuldu.

---
+++ Parking.Models/VehiclePhoto.cs
@@ -0,0 +1,9 @@
+﻿namespace Parking.Models
+{
+    public class VehiclePhoto : BaseModel
+    {
+        public string Path { get; set; }
+        public int VehicleId { get; set; }
+        public virtual Vehicle Vehicle { get; set; }
+    }
+}
