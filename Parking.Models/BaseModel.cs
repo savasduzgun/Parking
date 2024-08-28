c64f6c39574d67fe7b5715db0701b0c7f66140f6
savasduzgun <savasdzgn@gmail.com>
2024-08-28 14:23:49 +0300

    Base model oluşturuldu.

---
+++ Parking.Models/BaseModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Parking.Models
+{
+    public class BaseModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public Guid Guid { get; set; } = Guid.NewGuid();
+        public bool IsDeleted { get; set; } = false;
+        public bool IsActive { get; set; } = true;
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime? DateDeleted { get; set; }
+    }
+}
