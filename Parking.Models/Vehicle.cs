272bfa4488a4d4249abb04bd14717a9bdc348417
savasduzgun <savasdzgn@gmail.com>
2024-08-28 16:12:07 +0300

    Vehicle, VehicleProcess ve Type modelleri arasında ilişki kuruldu.

--- Parking.Models/Vehicle.cs
+++ Parking.Models/Vehicle.cs
@@ -10,5 +10,6 @@
         public int AppUserId { get; set; }
         public virtual AppUser AppUser { get; set; }
         public virtual ICollection<VehiclePhoto> VehiclePhotos { get; set; } = new List<VehiclePhoto>();
+        public virtual ICollection<VehicleProcess> VehicleProcesses { get; set; } = new List<VehicleProcess>();
     }
 }
---
+++ Parking.Models/VehicleProcessType.cs
@@ -0,0 +1,7 @@
+﻿namespace Parking.Models
+{
+    public class VehicleProcessType : BaseModel
+    {
+        public string Name { get; set; }
+    }
+}
