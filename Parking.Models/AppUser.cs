b97fb54d7b6688731f61cb5735df04e3eec0227c
savasduzgun <savasdzgn@gmail.com>
2024-08-28 15:43:14 +0300

    AppUser ve Vehicle modelleri arasındaki ilişki kuruldu.

--- Parking.Models/AppUser.cs
+++ Parking.Models/AppUser.cs
@@ -5,5 +5,6 @@
         public string FullName { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public virtual ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
     }
 }
--- Parking.Models/Vehicle.cs
+++ Parking.Models/Vehicle.cs
@@ -7,5 +7,7 @@
         public double Odometer { get; set; }
         public int VehicleTypeId { get; set; }
         public virtual VehicleType VehicleType { get; set; }
+        public int AppUserId { get; set; }
+        public virtual AppUser AppUser { get; set; }
     }
 }
