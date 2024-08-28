2b99f7d0297c7b34a3d2d45115611f8b5ab39517
savasduzgun <savasdzgn@gmail.com>
2024-08-28 16:44:44 +0300

    Policy ve PolicyType modelleri arasında ilişki kuruldu.

--- Parking.Models/Policy.cs
+++ Parking.Models/Policy.cs
@@ -5,5 +5,7 @@
         public string Name { get; set; }
         public string CompanyName { get; set; } //SigortaFirması
         public DateTime Validity { get; set; } //GeçerlilikTarihi
+        public int PolicyTypeId { get; set; }
+        public virtual PolicyType PolicyType { get; set; }
     }
 }
