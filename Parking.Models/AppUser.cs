5b65289083ddd668392715c67db941bab8270ec3
savasduzgun <savasdzgn@gmail.com>
2024-08-29 14:23:18 +0300

    AppUser modeline IsAdmin eklendi.

--- Parking.Models/AppUser.cs
+++ Parking.Models/AppUser.cs
@@ -5,6 +5,7 @@
         public string FullName { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public bool IsAdmin { get; set; } = false;
         public virtual ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
     }
 }
