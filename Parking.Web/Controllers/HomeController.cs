e863b6e5b70f0752e5e940d57600e52aec578934
savasduzgun <savasdzgn@gmail.com>
2024-09-02 18:02:52 +0300

    Login ekranı yapıldı.

--- Parking.Web/Controllers/HomeController.cs
+++ Parking.Web/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Parking.Web.Models;
 using System.Diagnostics;
 
 namespace Parking.Web.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
