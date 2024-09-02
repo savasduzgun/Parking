5dede6959b32c878b2f1faa0087851ea9c9d18ae
savasduzgun <savasdzgn@gmail.com>
2024-09-02 15:50:27 +0300

    UserController ve Login sayfasu için methodlar eklendi.

--- Parking.Web/Controllers/UserController.cs
+++ Parking.Web/Controllers/UserController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Parking.Data;
+using Parking.Models;
+using System.Security.Claims;
 
 namespace Parking.Web.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(AppUser appUser)
+        {
+            AppUser user = _context.Users.FirstOrDefault(u => u.UserName == appUser.UserName && u.Password == appUser.Password);
+            if (user != null)
+            {
+                List<Claim> claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+                claims.Add(new Claim(ClaimTypes.Name, user.UserName));
+                claims.Add(new Claim(ClaimTypes.GivenName, user.FullName));
+                claims.Add(new Claim(ClaimTypes.Role, user.IsAdmin ? "Admin" : "User"));
+
+                ClaimsIdentity identity = new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);
+
+                await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignInAsync(new ClaimsPrincipal(identity));
+
+                return RedirectToAction("Index", "Vehicle");
+            }
+
+            return View(); //Login sayfasına geri döner.
         }
     }
 }
