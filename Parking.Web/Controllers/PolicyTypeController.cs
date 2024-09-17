a62107283cd26abd779ddb1f33f9e883c2c8dafb
savasduzgun <savasdzgn@gmail.com>
2024-09-17 18:02:11 +0300

    Dependency injection yapıldı.

--- Parking.Web/Controllers/PolicyTypeController.cs
+++ Parking.Web/Controllers/PolicyTypeController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Parking.Data;
 
 namespace Parking.Web.Controllers
 {
     public class PolicyTypeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public PolicyTypeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
