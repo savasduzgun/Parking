caf85a4ac3d63cd6d22be2b65b5523e574eed3f7
savasduzgun <savasdzgn@gmail.com>
2024-10-05 15:11:49 +0300

    dependency injection yapıldı.

--- Parking.Web/Controllers/VehicleProcessTypeController.cs
+++ Parking.Web/Controllers/VehicleProcessTypeController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Parking.Data;
 
 namespace Parking.Web.Controllers
 {
     public class VehicleProcessTypeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public VehicleProcessTypeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
