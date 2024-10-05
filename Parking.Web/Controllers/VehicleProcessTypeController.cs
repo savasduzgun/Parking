bee14f7bc486bd7f72a4c84769c00683d84ea070
savasduzgun <savasdzgn@gmail.com>
2024-10-05 15:10:48 +0300

    VehicleProcessTypeController eklendi.

---
+++ Parking.Web/Controllers/VehicleProcessTypeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Parking.Web.Controllers
+{
+    public class VehicleProcessTypeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
