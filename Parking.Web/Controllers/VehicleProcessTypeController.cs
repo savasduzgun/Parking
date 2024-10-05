f6ce9b2d0a20eda154c40bba74450e9903f1d260
savasduzgun <savasdzgn@gmail.com>
2024-10-05 15:13:33 +0300

    GetAll yazıldı.

--- Parking.Web/Controllers/VehicleProcessTypeController.cs
+++ Parking.Web/Controllers/VehicleProcessTypeController.cs
@@ -16,5 +16,10 @@ namespace Parking.Web.Controllers
         {
             return View();
         }
+
+        public IActionResult GetAll()
+        {
+            return Json(new { Data = _context.VehicleProcessTypes.Where(vpt => !vpt.IsDeleted).ToList() });
+        }
     }
 }
