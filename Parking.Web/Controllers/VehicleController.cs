c63f99fd6b00f3b0bbff42aa2e470fd5a715cc78
savasduzgun <savasdzgn@gmail.com>
2024-10-06 14:18:23 +0300

    VehicleController eklendi.

---
+++ Parking.Web/Controllers/VehicleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Parking.Web.Controllers
+{
+    public class VehicleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
