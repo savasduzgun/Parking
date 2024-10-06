73f5f1107505ca0767f1fe04fa8f748ca4c3de4e
savasduzgun <savasdzgn@gmail.com>
2024-10-06 15:19:39 +0300

    dependency injection yapıldı.

--- Parking.Web/Controllers/VehicleController.cs
+++ Parking.Web/Controllers/VehicleController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Parking.Data;
 
 namespace Parking.Web.Controllers
 {
+    [Authorize]
     public class VehicleController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public VehicleController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
