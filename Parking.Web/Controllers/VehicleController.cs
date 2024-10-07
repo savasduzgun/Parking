fe0a88e804fb40a89436db148b1f448caf23db21
savasduzgun <savasdzgn@gmail.com>
2024-10-07 17:20:32 +0300

    açıklama için yorum satırları eklendi.

--- Parking.Web/Controllers/VehicleController.cs
+++ Parking.Web/Controllers/VehicleController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Parking.Data;
 using Parking.Models;
+using System.Numerics;
 using System.Security.Claims;
 
 namespace Parking.Web.Controllers
@@ -23,8 +24,10 @@ namespace Parking.Web.Controllers
 
         public IActionResult GettAll() 
         {
+            //Bu satırda, kullanıcı rolü kontrol ediliyor. Eğer giriş yapan kullanıcı Admin rolüne sahipse, aşağıdaki işlemler yapılacak. Aksi durumda, normal bir kullanıcı olarak kabul edilecek ve farklı bir sorgu çalıştırılacak.
             if (User.IsInRole("Admin"))
             {
+                //Eğer kullanıcı Admin ise, Vehicles (Araçlar) tablosundaki silinmemiş (IsDeleted = false) tüm araçlar sorgulanır.Select metodu ile her bir araçtan belli başlı bilgiler(adı, kilometre sayacı, plaka numarası, vb.) alınır ve result adında bir değişkene atanır.Admin kullanıcı tüm araçları görebilir, herhangi bir kullanıcıya bağlı olmasa bile.
                 var result = _context.Vehicles.Where(v => !v.IsDeleted).Select(v => new Vehicle
                 {
                     Name = v.Name,
@@ -38,6 +41,7 @@ namespace Parking.Web.Controllers
             }
             else 
             {
+                //Eğer kullanıcı Admin değilse, kullanıcının kimliği belirlenir. User.FindFirst(ClaimTypes.NameIdentifier) kullanılarak, giriş yapan kullanıcının ID’si elde edilir ve integer (tam sayı) formatına çevrilir. Bu kullanıcı ID’si appUserId değişkenine atanır. Sorgu, sadece bu kullanıcıya (AppUserId) ait ve silinmemiş (IsDeleted = false) araçları döndürmek üzere yapılır.
                 int appUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
                 var result = _context.Vehicles.Where(v => !v.IsDeleted && v.AppUserId == appUserId);
                 return Json(new { data = result });
