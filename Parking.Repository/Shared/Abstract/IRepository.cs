c180064563446f4248bfe669d29b731b9464baf8
savasduzgun <savasdzgn@gmail.com>
2024-10-10 22:26:39 +0300

    Generic yapıldı.

--- Parking.Repository/Shared/Abstract/IRepository.cs
+++ Parking.Repository/Shared/Abstract/IRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Parking.Repository.Shared.Abstract
 {
-    public interface IRepository
+    public interface IRepository<T> where T : class
     {
     }
 }
