b8c35ce5353e8288163e7a04fc06c7441f38c677
savasduzgun <savasdzgn@gmail.com>
2024-10-11 22:40:03 +0300

    VehicleProcessTypeController repository e taşındı.

--- Parking.Web/Controllers/VehicleProcessTypeController.cs
+++ Parking.Web/Controllers/VehicleProcessTypeController.cs
@@ -1,16 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Parking.Data;
 using Parking.Models;
+using Parking.Repository.Shared.Abstract;
 
 namespace Parking.Web.Controllers
 {
     public class VehicleProcessTypeController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        //private readonly ApplicationDbContext _context;
 
-        public VehicleProcessTypeController(ApplicationDbContext context)
+        //public VehicleProcessTypeController(ApplicationDbContext context)
+        //{
+        //    _context = context;
+        //}
+        private readonly IRepository<VehicleProcessType> _vehicleProcessTypeRepo;
+
+        public VehicleProcessTypeController(IRepository<VehicleProcessType> vehicleProcessTypeRepo)
         {
-            _context = context;
+            _vehicleProcessTypeRepo = vehicleProcessTypeRepo;
         }
 
         public IActionResult Index()
@@ -20,48 +28,54 @@ namespace Parking.Web.Controllers
 
         public IActionResult GetAll()
         {
-            return Json(new { Data = _context.VehicleProcessTypes.Where(vpt => !vpt.IsDeleted).ToList() });
+            //return Json(new { data = _context.VehicleProcessTypes.Where(vpt => !vpt.IsDeleted).ToList() });
+            return Json(new { data = _vehicleProcessTypeRepo.GetAll() });
         }
 
         [HttpPost]
         public IActionResult Add(VehicleProcessType vehicleProcessType)
         {
-            _context.VehicleProcessTypes.Add(vehicleProcessType);
-            _context.SaveChanges();
+            //_context.VehicleProcessTypes.Add(vehicleProcessType);
+            //_context.SaveChanges();
+            _vehicleProcessTypeRepo.Add(vehicleProcessType);
             return Ok(vehicleProcessType);
         }
 
         [HttpPost]
         public IActionResult Update(VehicleProcessType vehicleProcessType)
         {
-            _context.VehicleProcessTypes.Update(vehicleProcessType);
-            _context.SaveChanges();
+            //_context.VehicleProcessTypes.Update(vehicleProcessType);
+            //_context.SaveChanges();
+            _vehicleProcessTypeRepo.Update(vehicleProcessType);
             return Ok(vehicleProcessType);
         }
 
         [HttpPost]
         public IActionResult Delete(int id)
         {
-            var vehicleProcessType = _context.VehicleProcessTypes.Find(id);
-            vehicleProcessType.IsDeleted = true;
-            vehicleProcessType.DateDeleted = DateTime.Now;
-            _context.VehicleProcessTypes.Update(vehicleProcessType);
-            _context.SaveChanges();
+            //var vehicleProcessType = _context.VehicleProcessTypes.Find(id);
+            //vehicleProcessType.IsDeleted = true;
+            //vehicleProcessType.DateDeleted = DateTime.Now;
+            //_context.VehicleProcessTypes.Update(vehicleProcessType);
+            //_context.SaveChanges();
+            _vehicleProcessTypeRepo.DeleteById(id);
             return Ok();
         }
 
         [HttpPost]
         public IActionResult HardDelete(VehicleProcessType vehicleProcessType)
         {
-            _context.VehicleProcessTypes.Remove(vehicleProcessType);
-            _context.SaveChanges();
+            //_context.VehicleProcessTypes.Remove(vehicleProcessType);
+            //_context.SaveChanges();
+            _vehicleProcessTypeRepo.Delete(vehicleProcessType);
             return Ok();
         }
 
         [HttpPost]
         public IActionResult GetById(int id)
         {
-            return Ok(_context.VehicleProcessTypes.Find(id));
+            //return Ok(_context.VehicleProcessTypes.Find(id));
+            return Ok(_vehicleProcessTypeRepo.GetById(id));
         }
     }
 }
