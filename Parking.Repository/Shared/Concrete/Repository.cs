b134b3aee0598f77d3bc81a6337afaaa38a3d4b0
savasduzgun <savasdzgn@gmail.com>
2024-10-11 09:10:49 +0300

    dependency injection yapıldı.

--- Parking.Repository/Shared/Concrete/Repository.cs
+++ Parking.Repository/Shared/Concrete/Repository.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿using Parking.Data;
+using Parking.Repository.Shared.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Parking.Repository.Shared.Concrete
 {
-    public class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        private readonly ApplicationDbContext _context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public T Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T DeleteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
