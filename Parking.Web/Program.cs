bb791be3836e05d7cabd6fe839a7f868bbf6fd03
savasduzgun <savasdzgn@gmail.com>
2024-10-11 21:16:10 +0300

    Repository dependency injection ı için program.cs haberdar edildi.

--- Parking.Web/Program.cs
+++ Parking.Web/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using Parking.Data;
+using Parking.Repository.Shared.Abstract;
+using Parking.Repository.Shared.Concrete;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
 {
