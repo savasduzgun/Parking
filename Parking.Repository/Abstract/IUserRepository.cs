fc52fea1ae59409677c1f419682f073e1706c59a
savasduzgun <savasdzgn@gmail.com>
2024-10-15 17:33:54 +0300

    IUserRepository eklendi.

---
+++ Parking.Repository/Abstract/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Parking.Models;
+using Parking.Repository.Shared.Abstract;
+
+namespace Parking.Repository.Abstract
+{
+    public interface IUserRepository : IRepository<AppUser>
+    {
+    }
+}
