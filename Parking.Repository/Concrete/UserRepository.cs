38b85de6def387119b693fb0e0c6fcf554a19b21
savasduzgun <savasdzgn@gmail.com>
2024-10-15 17:34:07 +0300

    UserRepository eklendi.

---
+++ Parking.Repository/Concrete/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Parking.Data;
+using Parking.Models;
+using Parking.Repository.Abstract;
+using Parking.Repository.Shared.Concrete;
+
+namespace Parking.Repository.Concrete
+{
+    public class UserRepository : Repository<AppUser>, IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+        public override IQueryable<AppUser> GetAll()
+        {
+            return base.GetAll();
+        }
+    }
+}
